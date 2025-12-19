dd46157eb02bca8c7fd9057931c7161ac0b20fef
Jhanak Verma <vermajhanak2501@gmail.com>
2025-12-19 11:02:29 +0530

    Refactor data loader to use format-aware document loading

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,25 +1,16 @@
+from document_loader import load_document
 import os
 
-def load_text(file_path):
-    """
-    Reads text from a file and returns it as a string
-    """
-    with open(file_path, "r", encoding="utf-8") as file:
-        text = file.read()
-    return text
-
-
-def load_all_resumes(resume_folder):
+def load_all_resumes(folder_path):
     resumes = {}
-    for file_name in os.listdir(resume_folder):
-        file_path = os.path.join(resume_folder, file_name)
-        resumes[file_name] = load_text(file_path)
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
+        resumes[file_name] = load_document(file_path)
     return resumes
 
-
-def load_all_jobs(job_folder):
+def load_all_jobs(folder_path):
     jobs = {}
-    for file_name in os.listdir(job_folder):
-        file_path = os.path.join(job_folder, file_name)
-        jobs[file_name] = load_text(file_path)
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
+        jobs[file_name] = load_document(file_path)
     return jobs
